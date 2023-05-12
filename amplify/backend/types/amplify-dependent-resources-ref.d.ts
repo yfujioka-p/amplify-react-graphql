b751f529aea496b18ba7024ed2dacaaac5d52116
y.fujioka <CORP\y.fujioka@a-2k6ev4g9ujjyk.corp.amazonworkspaces.com>
2023-05-12 22:10:49 +0900

    add graphql storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,20 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "function": {
+    "S3Triggerb1b8abd2": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "s3e2fcb59b": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
